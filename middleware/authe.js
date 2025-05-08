a7fca2e4c8e471abcde116ec128ca732d2622e23
Fah <andriampanarivo.fahasoavana@gmail.com>
2025-05-08 17:50:00 +0300

    maintenant ok le profile

--- middleware/authe.js
+++ middleware/authe.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 module.exports = function (req, res, next){
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split('')[1];
+    const token = authHeader && authHeader.split(' ')[1];
     if(!token) return res.status(401).json({error: 'Token manquant'});
 
     try{
