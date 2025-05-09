4b42416a492ed20a4fedc4ba133e4503cd4ac2b4
Fah <andriampanarivo.fahasoavana@gmail.com>
2025-05-09 14:27:54 +0300

    recuperation mission par ID

--- routes/missions.js
+++ routes/missions.js
@@ -37,4 +37,26 @@ router.get('/showMissions', async(req, res)=>{
     }
 })
 
+//recuperation de mission par id pour postuler a une mission specifique
+router.get('/:id', async(req,res)=>{
+  console.log('Requete recue sur GET /api/missions/:id');
+
+  const missionId = req.params.id; //recupere id mission depuis parametre URL
+  try{
+    const result = await db.query('SELECT* FROM missions WHERE id_missions = $1', [missionId]);
+
+    if(result.rows.length === 0 ){
+      return res.status(404).json({error: 'Mission non trouvee'});
+    }
+
+    res.json(result.rows[0]); //renvoyer le resultat
+
+  } catch(err) {
+    console.error('Erreur lors de la recuperation de la mission par ID: ', err);
+    res.status(500).json({ error: 'Erreur serveur lors de la recuperation ID'});
+
+  }
+
+} )
+
 module.exports = router;
