1f03edeba6e9e3e16e558f1a78e39c3fec8921b8
Fah <andriampanarivo.fahasoavana@gmail.com>
2025-05-25 19:32:59 +0300

    add env.prod et env.dev

--- skillshub/src/pages/Login.jsx
+++ skillshub/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login (){
     e.preventDefault();
 
     try{
-      const res = await axios.post('/api/auth/login', {email, password} );
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}`, {email, password} );
 
        const {token}= res.data;
        setToken(token);
